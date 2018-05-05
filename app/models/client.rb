0318f635da688ca1e41fc5b39798be3920fd8656
Jean Brito <jeanfbrito@gmail.com>
2018-05-04 21:34:42 -0300

    add contacts inside client form

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,5 @@
 class Client < ApplicationRecord
+  has_many :contacts, inverse_of: :client
+  accepts_nested_attributes_for :contacts, allow_destroy: true
+  has_many :products, through: :sales
 end
