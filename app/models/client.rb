2cd85293ed20b9a0d009e33894504418cea9e58b
Jean Brito <jeanfbrito@gmail.com>
2018-05-04 23:21:32 -0300

    add products to clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,9 @@
 class Client < ApplicationRecord
   has_many :contacts, inverse_of: :client
   accepts_nested_attributes_for :contacts, allow_destroy: true
+  has_many :sales
   has_many :products, through: :sales
+  
+  accepts_nested_attributes_for :products
+  accepts_nested_attributes_for :sales
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,5 @@
 class Sale < ApplicationRecord
   belongs_to :client
   belongs_to :product
+  accepts_nested_attributes_for :product
 end
